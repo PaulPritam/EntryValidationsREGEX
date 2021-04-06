9ad002dfb9f0fe45961e7c695c288fca41bb099b
unknown <pritampaul9798@gmail.com>
2021-04-06 14:59:11 +0530

    added regex to add last name

--- Regex.java
+++ Regex.java
@@ -4,21 +4,31 @@ import java.util.*;
 import java.util.regex.*;
 
 public class Regex {
-	public static boolean firstname(String s) {
+	public static boolean name(String t) {
 
-		Pattern p = Pattern.compile("[A-Z]{1}[a-z]{3,}");
-		Matcher m = p.matcher(s);
-		return (m.find() && m.group().equals(s));
+		Pattern p = Pattern.compile("[A-Z]{1}[a-z]{2,}");
+		Matcher m = p.matcher(t);
+		return (m.find() && m.group().equals(t));
 	}
-
 
 	public static void main(String[] args) {
 		System.out.println("Enter first name ");
 		Scanner sc = new Scanner(System.in);
 		String f = sc.nextLine();
-		if (firstname(f))
+		if (name(f))
 			System.out.println("first name is valid");
 
 		else
+
 			System.out.println("first name is invalid");
+
+		System.out.println("\nenter last name ");
+		String l = sc.nextLine();
+		if (name(l))
+			System.out.println("last name is valid");
+
+		else
+			System.out.println("last name is invalid");
+
+	}
 }
