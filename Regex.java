3e8267d30f178198d5c4c9cba305a12628b7de40
unknown <pritampaul9798@gmail.com>
2021-04-06 15:01:46 +0530

    added regex for email

--- Regex.java
+++ Regex.java
@@ -11,6 +11,13 @@ public class Regex {
 		return (m.find() && m.group().equals(t));
 	}
 
+	public static boolean email(String e) {
+
+		Pattern p = Pattern.compile("([A-Za-z]{1,10}[\\.])([A-Za-z]{1,10})?([@][b][l][\\.][c][o][\\.])([in]{2})?");
+		Matcher m = p.matcher(e);
+		return (m.find() && m.group().equals(e));
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Enter first name ");
 		Scanner sc = new Scanner(System.in);
@@ -19,7 +26,7 @@ public class Regex {
 			System.out.println("first name is valid");
 
 		else
-
+			
 			System.out.println("first name is invalid");
 
 		System.out.println("\nenter last name ");
@@ -30,5 +37,12 @@ public class Regex {
 		else
 			System.out.println("last name is invalid");
 
+		System.out.println("\nenter email ");
+		String e = sc.nextLine();
+		if (email(e))
+			System.out.println("email is valid");
+
+		else
+			System.out.println("email is invalid");
 	}
 }
